81e3e0c5e544ef36cee9743406ff918fe7ebce22
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-30 21:30:24 +0500

    install devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,25 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
-  def most_recent_posts
-    posts.order(created_at: :desc).limit(3)
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
+  def all_posts
+    posts.includes([:author]).order(created_at: :desc)
+  end
+
+  def recent_posts
+    all_posts.limit(3)
   end
 end
