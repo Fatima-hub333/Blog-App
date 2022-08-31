2312daf05e894eab303ce2c7219a47bc1e41c638
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 11:18:07 +0500

    Add delete methods for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,36 @@
 class CommentsController < ApplicationController
-  include ApplicationHelper
+  load_and_authorize_resource
+  def new
+    @comment = Comment.new
+  end
 
   def create
-    @comment = Comment.new(params.require(:comment).permit(:author_id, :post_id, :text))
-
-    author_id = @comment.author_id
-    post_id = @comment.post.id
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post = Post.find(params[:post_id])
+    @author = User.find(params[:user_id])
 
     if @comment.save
-      flash[:notice] = 'Comment has been saved successfully'
-      redirect_to user_post_path(author_id, post_id)
-
+      flash[:success] = 'Comment created!'
+      redirect_to user_post_path(@author, @comment.post_id)
     else
-      flash[:error] = 'Comment could not be saved'
-      redirect_to new_user_post_comment_path(author_id, post_id)
+      flash.now[:error] = 'Comment not created! Try again.'
+      render :new
     end
   end
 
-  def new
-    @comment = Comment.new(author_id: current_user.id, post_id: params[:post_id])
+  def destroy
+    post = Post.find(params[:post_id])
+    user_id = post.author_id
+    id = @comment.post_id
+    @comment.destroy
+
+    redirect_to user_post_path(user_id, id)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :author_id, :post_id)
   end
 end
