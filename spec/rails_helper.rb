967dfda3cd7bae42508ac2d41c26c96159b366de
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 15:26:55 +0500

    Update rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
+require 'webdrivers'
+require 'selenium-webdriver'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -26,73 +29,66 @@ require 'capybara/rspec'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
-  Capybara.register_driver :selenium_chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
-  end
-
-  Capybara.javascript_driver = :selenium_chrome
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-# You can uncomment this line to turn off ActiveRecord support entirely.
-# config.use_active_record = false
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
 
-# RSpec Rails can automatically mix in different behaviours to your tests
-# based on their file location, for example enabling you to call `get` and
-# `post` in specs under `spec/controllers`.
-#
-# You can disable this behaviour by removing the line below, and instead
-# explicitly tag your specs with their type, e.g.:
-#
-#     RSpec.describe UsersController, type: :controller do
-#       # ...
-#     end
-#
-# The different available types are documented in the features, such as in
-# https://relishapp.com/rspec/rspec-rails/docs
-config.infer_spec_type_from_file_location!
+  # You can uncomment this line to turn off ActiveRecord support entirely.
+  # config.use_active_record = false
 
-# Filter lines from Rails gems in backtraces.
-config.filter_rails_from_backtrace!
-# arbitrary gems may also be filtered via:
-# config.filter_gems_from_backtrace("gem name")
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, type: :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
 
-config.before(:each, type: :system) do
-  driven_by :selenium_chrome
-end
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
 
-config.before(:suite) do
-  DatabaseCleaner.clean_with(:truncation)
-end
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Warden::Test::Helpers
 
-config.before(:each) do
-  DatabaseCleaner.strategy = :transaction
-end
-
-config.before(:each, js: true) do
-  DatabaseCleaner.strategy = :truncation
-end
-
-# This block must be here, do not combine with the other `before(:each)` block.
-# This makes it so Capybara can see the database.
-config.before(:each) do
-  DatabaseCleaner.start
-end
-
-config.after(:each) do
-  DatabaseCleaner.clean
-end
-
-# spec/rails_helper.rb
-if Bullet.enable?
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
   config.before(:each) do
-    Bullet.start_request
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  # This block must be here, do not combine with the other `before(:each)` block.
+  # This makes it so Capybara can see the database.
+  config.before(:each) do
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
-    Bullet.perform_out_of_channel_notifications if Bullet.notification?
-    Bullet.end_request
+    DatabaseCleaner.clean
   end
 end
+Capybara.configure do |config|
+  config.run_server = false
+end
+session = Capybara::Session.new(:selenium)
