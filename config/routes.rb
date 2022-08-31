36b7b05d99c686498b81f1e6331172784059debc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 16:14:39 +0500

    Fix issue in Routes.rb (suggested by code-Reviwer)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :html },
-                         controllers: {
-                         sessions: 'users/sessions'
-                        }
-  
-  get 'sign_in', to: 'users/sessions#new'
+  devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show]
 
@@ -12,14 +7,6 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
-    end
-  end
-  
-  namespace :api do
-    resources :users, only: [] do
-      resources :posts, only: [:index], format: :json do
-        resources :comments, only: [:index, :create], format: :json
-      end
     end
   end
 end
