f17e64f9706065ecf631c316cdabdf1e2749c1ae
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 11:20:36 +0500

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,15 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
-  after_save :update_comments_count
+  after_save :update_comments_counter
+  after_destroy :update_comments_counter
+
+  validates :text, presence: true, allow_blank: false, length: { maximum: 250 }
 
   private
 
-  def update_comments_count
-    post.increment!(:comments_count)
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
 end
