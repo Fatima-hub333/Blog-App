f4f1190c2ca1f3cb25269f4aaee42926187ccaef
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 16:24:21 +0500

    Fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users, defaults: { format: :html }
+  
+  get 'sign_in', to: 'users/sessions#new'
+  root 'users#index'
+  resources :users, only: [:index, :show]
 
   resources :users, only: [:index, :show, :new, :create] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
+    end
+  end
+  
+  namespace :api do
+    resources :users, only: [] do
+      resources :posts, only: [:index], format: :json do
+        resources :comments, only: [:index, :create], format: :json
+      end
     end
   end
 end
