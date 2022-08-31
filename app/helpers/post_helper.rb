86a1004d576d61cc07993992a105912fb70a32fd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 13:52:10 +0500

    re-organize files in order to fix bug in project

--- app/helpers/post_helper.rb
+++
@@ -1,9 +0,0 @@
-module PostHelper
-  def with_comments?(post)
-    post.comments_count.positive? && !current_page?(user_path(post.author_id))
-  end
-
-  def short_text?(post)
-    !current_page?(user_post_path(post.author_id, post.id))
-  end
-end
