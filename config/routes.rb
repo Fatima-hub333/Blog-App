3b914a779fd8c0921ad9bc605301f3cb9821f0fa
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-31 11:26:09 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :tests
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show]
 
   resources :users, only: [:index, :show, :new, :create] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
